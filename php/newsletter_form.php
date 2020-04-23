6e80ab1cbf30dc22c9731e2d03ffc7089c9ed1e1
Liam Arbuckle <allianceofdroidsla@gmail.com>
2020-04-23 21:07:52 +0800

    Update newsletter_form.php

--- php/newsletter_form.php
+++ php/newsletter_form.php
@@ -6,7 +6,7 @@
 
         $newsletter = $_POST["newsletter"];
 
-        $mailbody = "
+        $mailbody = 
                 <head>
                     <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
                 </head>
