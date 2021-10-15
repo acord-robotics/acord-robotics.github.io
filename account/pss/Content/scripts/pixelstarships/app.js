f0e905f658ff766b37dbb7c7072c8777a1f2dd99
Gizmotronn <droidology@allianceofdroids.org.au>
2021-10-15 14:25:52 +0800

    Add SavySoda>PSS

---
+++ account/pss/Content/scripts/pixelstarships/app.js
@@ -0,0 +1,44 @@
+﻿var pixelstarshipsApp = angular.module('PixelStarshipsApp', []);
+
+pixelstarshipsApp.controller('subscriptionController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.emailAddress = "";
+
+    
+    $scope.subscribe = function () {
+                
+        /*
+        var data = $.param({
+            json: JSON.stringify({
+                emailAddress: $scope.emailAddress
+            })
+        }); */
+        /*
+        $http.post("/subscribe", data).success(function (data, status) {
+            //$scope.status = data;
+            $scope.status = 'You have subscribed to Pixel Starships successfully.';
+        }).error(function(error)
+        {
+            //$scope.status = error.message;
+            $scope.status = "You already subscribed to Pixel Starships.";
+        }); */
+
+        $http.post('subscribe', { emailAddress: $scope.emailAddress })
+        .success(function (data, status, headers, config) {
+            //alert(data.statusText);
+            // some code here
+            $scope.status = data.message;            
+            //alert(data.message);
+        })
+        .catch(function (data, status, headers, config) { // <--- catch instead error
+            //alert(data.statusText);
+            //data.statusText; //contains the error message
+            $scope.status = "An error occured.";
+            
+
+        });
+
+    };
+
+    
+}]);
